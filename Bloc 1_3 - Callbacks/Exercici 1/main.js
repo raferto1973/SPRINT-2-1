39423035cb7c9a09cec791326c49bf22c5e7ce7c
raferto1973 <infowebs.rafa@gmail.com>
2023-11-27 19:30:32 +0100

    Bloc 1_3 Inici

---
+++ Bloc 1_3 - Callbacks/Exercici 1/main.js
@@ -0,0 +1,2 @@
+//Callback bàsic: Escriu una funció anomenada processar que accepti dos paràmetres: un nombre i una funció de callback. La funció processar ha d'invocar la funció de callback, passant el nombre com a paràmetre.
+
